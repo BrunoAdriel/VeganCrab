cc22d9b4a44cdda2b60f75d7deda4e3e59dabaee
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-09-08 17:44:19 -0300

    Inicio con el Front, navbar

---
+++ Front/Antojo-Vegano/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NavigationBar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <>
+    <NavigationBar></NavigationBar>
+      <div>
+        <h1>Hola mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
