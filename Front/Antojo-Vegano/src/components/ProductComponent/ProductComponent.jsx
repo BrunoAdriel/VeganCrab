b133a7b4599211d14638fab5b5cb135bf1684948
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-09-30 21:41:14 -0300

    Agrego el btn de buscarde manera dinamica, falta modificarpara los productos

--- Front/Antojo-Vegano/src/components/ProductComponent/ProductComponent.jsx
+++ Front/Antojo-Vegano/src/components/ProductComponent/ProductComponent.jsx
@@ -1,21 +1,34 @@
 import React, {useState, useEffect} from "react";
 import './ProductComponent.css';
 import ProductCard from "../ProductCard/ProductCard.jsx";
+import SearchBar from "../SearchBar/SearcBar";
 
 const ProductComponent = () =>{
     const [products, setProducts] = useState([]);
+    
 
-            useEffect(()=>{fetch("http://localhost:3000/products")
+        useEffect(()=>{fetch("http://localhost:3000/products")
             .then((res)=> res.json())
             .then((data)=> {console.log("Datos sobre Productos:", data.products); setProducts(data.products); })
             .catch((error) => console.error("Error en el Fetch de Productos", error));
         },[]);
     
-    
-    
+
+
     return(<>
+        {/* Titulo de Productos */}
+        <div className="section-title">
+            <span></span>
+            <h2>Nuestros Productos</h2>
+            <span></span>
+        </div>
     
-        
+    <div className="container">
+        <div>
+            <SearchBar   />
+        </div>
+
+    </div>
 
 
     </>)
