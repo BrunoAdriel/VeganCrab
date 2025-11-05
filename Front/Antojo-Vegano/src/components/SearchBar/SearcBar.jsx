be27b4ffbe99d62bc545daf1deae255d50f67c9b
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-11-05 16:48:26 -0300

    ya busca los productos y devuelve toda su informacion,falta  ajustarlos bien sobre su contenido y modificar la info que aparece

--- Front/Antojo-Vegano/src/components/SearchBar/SearcBar.jsx
+++ Front/Antojo-Vegano/src/components/SearchBar/SearcBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SearchBar.css';
 
 const SearchBar = ({ variant = "nav", onSearch }) => {
@@ -8,47 +9,68 @@ const SearchBar = ({ variant = "nav", onSearch }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("Ejecutando búsqueda con:", query);
         if (!query.trim()) return;
 
         try {
             const res = await fetch(`http://localhost:3000/products/search?q=${encodeURIComponent(query)}`);
             const data = await res.json();
-            console.log("Resultado:", data);
-            setResults(data.filter || []); // Ajustá según tu estructura de respuesta
+            setResults(data.filter || []); 
         } catch (error) {
             console.error("Error al buscar:", error);
         }
     };
 
+    /* Cuando se clickea el producto */
+    const handleSelect = () => {
+        setQuery("");
+        setResults([]);
+        setShowSearch(false);
+        window.scrollTo({ top:0, behavior: "smooth"});
+    }
+
     return (
         <div className={`search-container ${variant}`}>
-        {/* Botón para mobile */}
+            {/* Botón para mobile */}
             {variant === "nav" && !showSearch && (
                 <button className="icon-btn only-mobile" aria-label="Abrir búsqueda" onClick={() => setShowSearch(true)}>
                     <span className='icon'>🔍</span>
                 </button>
             )}
-        <form className={`search-box ${variant === "nav" ? (showSearch ? "open" : "") : "always-open"}`} onSubmit={handleSubmit}>
-            {variant === "nav" && (
-                <button type="button" className="icon-btn only-mobile me-2" aria-label="Volver" onClick={() => setShowSearch(false)}> ← </button>
-            )}
-            <input type="text" className="search-input" placeholder="Buscar" value={query}onChange={(e) => setQuery(e.target.value)}/>
-            <button type="submit" className="icon-btn end-icon" aria-label="Buscar">
-                <span className='icon'>🔍</span>
-            </button>
-        </form>
+            {/* Formulario de búsqueda */}
+            <form className={`search-box ${variant === "nav" ? (showSearch ? "open" : "") : "always-open"}`} onSubmit={handleSubmit}>
+                {variant === "nav" && (
+                    <button type="button" className="icon-btn only-mobile me-2" aria-label="Volver" onClick={() => setShowSearch(false)}> ← </button>
+                )}
+                <input type="text" className="search-input" placeholder="Buscar" value={query}onChange={(e) => setQuery(e.target.value)}/>
+                <button type="submit" className="icon-btn end-icon" aria-label="Buscar">
+                    <span className='icon'>🔍</span>
+                </button>
+            </form>
 
-        {/* En el caso de encontrar el producto */}
-        {results.length > 0 && (
-            <ul className="search-results">
-                {results.map((item) => (
-                    
-                    <li key={item.idProduct}>{item.prodName} - ${item.unit_price}</li>
-                ))}
-            </ul>
-        )}
-    </div>
+            {/* En el caso de encontrar el producto */}
+            {results.length > 0 && (
+                <div className="search-results-box">
+                    <ul className="search-results">
+                        {results.map((prod) => (
+                            
+                            <li key={prod.idProduct}>
+                            <Link to={`/products/${prod.idProduct}`} onClick={handleSelect} className="search-item-link" >
+                                <div className="search-item">
+                                    <img src={ prod.images ? prod.images.split("|")[0] : "/placeholder.jpg" } alt={prod.prodName} />
+                                    <div className="search-info">
+                                        <h6>{prod.prodName}</h6>
+                                        <p>{prod.prodDescription}</p>
+                                        <p>${prod.minPrice?.toLocaleString() || "Consultar"}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </div>
+            )}
+        </div>
 );
 };
 
